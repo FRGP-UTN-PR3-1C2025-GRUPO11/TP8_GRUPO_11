6cac5a3e852be0536fe01edaedd876f69f094ef5
Mariano Belgrano <mbelgrano@gmail.com>
2025-05-28 18:41:19 -0300

    Agrego AccesoDatos, DaoSucursal

--- Datos/AccesoDatos.cs
+++ Datos/AccesoDatos.cs
@@ -3,10 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data
+using System.Data.SqlClient
 
 namespace Datos
 {
-    internal class AccesoDatos
+    public class AccesoDatos
     {
+        private const string cadenaConexion = @"DataSource=localhost\SQLEXPRESS;Initial Catalog=BDSucursales;Integrated Security=True;Encrypt=True;Trust Server Certificate=True"
+
+        
+        public AccesoDatos() { }
+
+        
+        private SqlConnection cn = new SqlConnection(cadenaConexion);
+
+        
+        private SqlDataAdapter ObtenerAdaptador(String consultaSql, SqlConnection cn)
+        {
+            SqlDataAdapter adaptador;
+            try
+            {
+                adaptador = new SqlDataAdapter(consultaSql, cn);
+                return adaptador;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public DataTable ObtenerTabla(String NombreTabla, String Sql)
+        {
+            DataSet ds = new DataSet();
+            SqlConnection Conexion = ObtenerConexion();
+            SqlDataAdapter adp = ObtenerAdaptador(Sql, Conexion);
+            adp.Fill(ds, NombreTabla);
+            Conexion.Close();
+            return ds.Tables[NombreTabla];
+        }
+
+        public int EjecutarProcedimientoAlmacenado(SqlCommand Comando, String NombreSP)
+        {
+            int FilasCambiadas;
+            SqlConnection Conexion = ObtenerConexion();
+            SqlCommand cmd = new SqlCommand();
+            cmd = Comando;
+            cmd.Connection = Conexion;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = NombreSP;
+            FilasCambiadas = cmd.ExecuteNonQuery();
+            Conexion.Close();
+            return FilasCambiadas;
+        }
+
+        public Boolean existe(String consulta)
+        {
+            Boolean estado = false;
+            SqlConnection Conexion = ObtenerConexion();
+            SqlCommand cmd = new SqlCommand(consulta, Conexion);
+            SqlDataReader datos = cmd.ExecuteReader();
+            if (datos.Read())
+            {
+                estado = true;
+            }
+            return estado;
+        }
+
+        public int ObtenerMaximo(String consulta)
+        {
+            int max = 0;
+            SqlConnection Conexion = ObtenerConexion();
+            SqlCommand cmd = new SqlCommand(consulta, Conexion);
+            SqlDataReader datos = cmd.ExecuteReader();
+            if (datos.Read())
+            {
+                max = Convert.ToInt32(datos[0].ToString());
+            }
+            return max;
+        }
+
     }
 }
---
+++ Datos/DaoSucursal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class DaoSucursal
+    {
+        AccesoDatos ds = new AccesoDatos();
+        public Sucursal getSucursal(Sucursal sucursal)
+        {
+            DataTable tabla = ds.ObtenerTabla("SUCURSAL", "SELECT * FROM SUCURSAL WHERE ID_SUCURSAL =" + sucursal.getIdSucursal());
+            sucursal.setIdSucursal(Convert.ToInt32(tabla.Rows[0][0].ToString()));
+            sucursal.setNombreSucursal(tabla.Rows[0][1].ToString());
+            sucursal.setDescripcionSucursal(tabla.Rows[0][2].ToString());
+            return sucursal;
+        }
+
+        public Boolean existeSucursal(Sucursal sucursal)
+        {
+            string consulta = "SELECT * FROM SUCURSAL WHERE ID_SUCURSAL='" + sucursal.getNombreSucursal() + "'";
+            return ds.existe(consulta);
+        }
+
+        public DataTable getTablaSucursales()
+        {
+            // List<Sucursal> lista = new List<Sucursal>();
+            DataTable tabla = ds.ObtenerTabla("Sucursal", "SELECT * FROM SUCURSAL");
+            return tabla;
+        }
+
+        public int eliminarSucursal(Sucursal sucursal)
+        {
+            SqlCommand comando = new SqlCommand();
+            ArmarParametrosSucursalEliminar(ref comando, sucursal);
+            return ds.EjecutarProcedimientoAlmacenado(comando, "SP_EliminarSucursal");
+        }
+
+
+        public int agregarSucursal(Sucursal sucursal)
+        {
+
+            sucursal.setId_Sucursal(ds.ObtenerMaximo("SELECT max(id_Sucursal) FROM Sucursal") + 1);
+            SqlCommand comando = new SqlCommand();
+            ArmarParametrosSucursalAgregar(ref comando, sucursal);
+            return ds.EjecutarProcedimientoAlmacenado(comando, "SP_AgregarSucursal");
+        }
+
+        private void ArmarParametrosSucursalEliminar(ref SqlCommand Comando, Sucursal sucursal)
+        {
+            SqlParameter SqlParametros = new SqlParameter();
+            SqlParametros = Comando.Parameters.Add("@IDSUCURSAL", SqlDbType.Int);
+            SqlParametros.Value = sucursal.getIdSucursal();
+        }
+
+        private void ArmarParametrosSucursalAgregar(ref SqlCommand Comando, Sucursal sucursal)
+        {
+            SqlParameter SqlParametros = new SqlParameter();
+            SqlParametros = Comando.Parameters.Add("@IDSUCURSAL", SqlDbType.Int);
+            SqlParametros.Value = sucursal.getIdSucursal();
+            SqlParametros = Comando.Parameters.Add("@NOMBRESUCURSAL", SqlDbType.VarChar);
+            SqlParametros.Value = sucursal.getNombreSucursal();
+        }
+    }
+}
